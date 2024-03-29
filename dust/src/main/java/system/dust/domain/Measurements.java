22464767b599d2a369312cd1e962ec6764545096
hyeri6318 <lhl6318@naver.com>
2024-03-30 00:04:26 +0900

    경보단계 등급표 Entity 구현

---
+++ dust/src/main/java/system/dust/domain/Measurements.java
@@ -0,0 +1,21 @@
+package system.dust.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 경보 단계 등급표 Entity
+ * */
+
+@Entity
+@Getter
+@Setter
+public class Measurements {
+
+    @Id
+    private int grade;  // 등급
+
+    private String steps;   // 경보단계
+}

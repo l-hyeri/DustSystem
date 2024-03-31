85181198b7d51f0e14a07ee23ba4f4dd857cd041
hyeri6318 <lhl6318@naver.com>
2024-03-31 19:39:38 +0900

    Json파일 load하는 클래스 수정

--- dust/src/main/java/system/dust/InitJsonLoader.java
+++ dust/src/main/java/system/dust/InitJsonLoader.java
@@ -1,4 +1,4 @@
-package system.dust.controller;
+package system.dust;
 
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -10,13 +10,12 @@ import system.dust.service.JsonFileReader;
 import java.util.List;
 
 /**
- * spring에서 controller 역할을 하는 클래스로
- * 해당 프로젝트에서는 json파일을 읽고 DB에 저장하도록 연결하는 역할을 하는 클래스
+ * json파일을 읽고 DB에 저장하도록 연결하는 역할을 하는 클래스
  * */
 
 @Component
 @AllArgsConstructor
-public class Controller implements CommandLineRunner {
+public class InitJsonLoader implements CommandLineRunner {
 
     private final AnalyzeService analyzeService;
 
